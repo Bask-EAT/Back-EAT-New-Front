0dec0db7e477e3cba1eb527dac2c66310c239d39
seulgiLee113 <cktncth55@gmail.com>
2025-08-27 15:02:35 +0900

    docs/ next.config.mjs 충돌 수정

--- next.config.mjs
+++ next.config.mjs
@@ -9,12 +9,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-<<<<<<< HEAD
-=======
   env: {
     NEXT_PUBLIC_BACKEND_BASE: process.env.NEXT_PUBLIC_BACKEND_BASE || 'http://localhost:8080',
   },
->>>>>>> 36958c5 (docs/ 로컬 프로젝트 초기 셋업)
 }
 
 export default nextConfig
